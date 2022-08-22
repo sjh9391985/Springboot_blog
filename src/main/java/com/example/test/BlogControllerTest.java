12a05e25dfaafbacdadb2639301744c32c5da1ac
sjh9391985 <sjh9391985@gmail.com>
2022-08-22 15:20:15 +0900

    springboot blog settings

---
+++ src/main/java/com/example/test/BlogControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * class 설명:
+ * packageName : com.example.test
+ * fileName : BlogControllerTest
+ * author : sonjaehyeon
+ * date : 2022/08/22
+ */
+
+@Controller
+public class BlogControllerTest {
+
+    @GetMapping("/test/hello")
+    public String Hello(){
+        return "Hello Spring";
+    }
+
+}
