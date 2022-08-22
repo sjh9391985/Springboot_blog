9546c30db38e79d060a2cfe1cf5c21a661b1353d
sjh9391985 <sjh9391985@gmail.com>
2022-08-22 17:30:07 +0900

    jsp settings

---
+++ src/main/java/com/example/demo/test/TempControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * class 설명:
+ * packageName : com.example.demo.test
+ * fileName : TempControllerTest
+ * author : sonjaehyeon
+ * date : 2022/08/22
+ */
+@Controller
+public class TempControllerTest {
+
+    @GetMapping("/temp/home")
+    public String tempHome(){
+
+        // 기본 return 명은 src/main/resources/static 이므로 앞에 / 를 붙여줘야함.
+        return "/home.html";
+    }
+
+    @GetMapping("/temp/jsp")
+    public String tempJsp(){
+
+        // 기본 return 명은 src/main/resources/static 이므로 앞에 / 를 붙여줘야함.
+        return "/test";
+    }
+}
